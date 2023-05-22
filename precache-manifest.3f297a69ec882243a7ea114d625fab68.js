4be428ea492b4d125c01a6a2588cabd7a912b72c
buithuytien <buithuytien13.13@gmail.com>
2023-05-22 18:39:09 -0400

    Updates

---
+++ precache-manifest.3f297a69ec882243a7ea114d625fab68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "32c9d48cbf01bf0c079e",
+    "url": "/me-portfolio/static/css/main.f8d47323.chunk.css"
+  },
+  {
+    "revision": "32c9d48cbf01bf0c079e",
+    "url": "/me-portfolio/static/js/main.32c9d48c.chunk.js"
+  },
+  {
+    "revision": "794debf2b66faec36961",
+    "url": "/me-portfolio/static/js/runtime~main.794debf2.js"
+  },
+  {
+    "revision": "5972a582c85a7ffc5352",
+    "url": "/me-portfolio/static/js/2.5972a582.chunk.js"
+  },
+  {
+    "revision": "4e0d2ffba476e920e7795e0bc9fef193",
+    "url": "/me-portfolio/index.html"
+  }
+];
